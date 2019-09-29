6599be921b45bda960f6ffa2d957a9b2b4b47247
Mesut <50662174+Efiloglu@users.noreply.github.com>
2019-09-29 15:09:10 +0200

    Update db.js

--- week-2-homework/todos-app/src/db.js
+++ week-2-homework/todos-app/src/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
-  password: 'hyfpassword',
+  password: 'Hyfpassword35',
   database: 'hyf_c5_w1_cars'
 });
 
